aec1294fa98c89c63600ab664f211e8f2efc96af
smabbasht <abbastaqvi81@gmail.com>
2024-03-13 10:43:50 +0500

    fix views.py

--- segmentation/views.py
+++ segmentation/views.py
@@ -1,33 +1,24 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-from django.core.files.base import ContentFile
 from django.conf import settings
 import os
-import time
 
 
 # Simulated image processing function
 def segment_image(image):
-    # For now, it simply returns the image file as is
-    # Replace this with your actual image processing logic
-    # add 3 secs wait
-    return image
+    # @Hussain add your logic here, return the processed_image
+    processed_image = image  # do something here
+    return processed_image
 
 
 @csrf_exempt
 def process_image(request):
     if request.method == 'POST' and request.FILES['image']:
-        # Read the uploaded image file
         image = request.FILES['image']
 
-        # Simulate image processing (replace with actual logic later)
         processed_image = segment_image(image)
-
-        # Define path for saving the processed image
         save_path = os.path.join(settings.MEDIA_ROOT, 'model_output.png')
-
-        # Ensure the directory exists
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
 
         # Save the "processed" image to the specified path
@@ -35,11 +26,9 @@ def process_image(request):
             for chunk in processed_image.chunks():
                 destination.write(chunk)
 
-        # Generate the URL for the saved image
         processed_image_url = os.path.join(
             settings.MEDIA_URL, 'model_output.png')
 
-        # Respond with the URL to the processed image
         return JsonResponse({
             'processed': processed_image_url,
         })
